3473932ba0cb00be9e934f3742d088393a3a789b
Octantis <none@notset.com>
2013-06-25 16:00:12 -0500

    Not working although storing here before trying gtk-cffi again

--- lispkit.lisp
+++ lispkit.lisp
@@ -2,16 +2,13 @@
   (:use :cl :gtk)
   (:shadow #:yes-or-no-p #:y-or-n-p))
 (in-package :lispkit)
+(export '(lispkit))
 
 ;; (in-package :lispkit)
 ;; LISPKIT> (webkit.foreign:webkit-web-view-get-uri (first *views*))
 
-(webkit.foreign:webkit-web-view-get-settings (first *views*))
-
-(gobject:g-object-get-data (webkit.foreign:webkit-web-view-new) "enable-plugins")
-
-
-(export '(lispkit))
+;; (webkit.foreign:webkit-web-view-get-settings (first *views*))
+;; (gobject:g-object-get-data (webkit.foreign:webkit-web-view-new) "enable-plugins")
 
 (defvar *swank-server*)
 
@@ -27,89 +24,72 @@
 (defvar *uri-homepage* "http://10.1.7.1/startpage/index.html"
   "The homepage uri to load by default")
 
+;; XXX: WERKS!
+;; LISPKIT> (gobject:get-gobject-property (webkit.foreign:webkit-web-settings-new)
+;;                             "html5-local-storage-database-path" gobject:+g-type-string+)
+;; will have to expornt the set/get func -- ask patch
+;; LISPKIT> (gobject:set-gobject-property (gobject:pointer (gobject:get-gobject-property (first *views*) "window-features")) "scrollbar-visible" nil)
+;; LISPKIT> (gobject:get-gobject-property (webkit.foreign:webkit-web-view-get-main-frame (first *views*)) "vertical-scrollbar-policy")
 
-(defun change-settings (settings-pointer opts)
-  "Given a GObject pointer for the WebKitGTK Settings object
-Use that to change it by the given settings"
-  (mapcar (lambda (setting-list-item)
-            (let ((property (first setting-list-item))
-                  (type (second setting-list-item))
-                  (value (third setting-list-item)))
-              (cffi:with-foreign-objects ((p 'gobject:g-value))
-                (gobject:set-g-value p value type :g-value-init t)
-                (gobject:g-object-set-data settings-pointer
-                                           property p))))
-          opts)
-    settings-pointer)
-
+(defun change-settings (view opts)
+  "Supply a view pointer. Use that to change it by the given settings.
+Must supply valid props"
+  (let ((settings-pointer (webkit.foreign:webkit-web-settings-new)))
+    (mapcar (lambda (setting-list-item)
+              (let ((property (first setting-list-item))
+                    (value (second setting-list-item)))
+                (gobject:set-gobject-property settings-pointer property
+                                              value)))
+            opts)
+    (webkit.foreign:webkit-web-view-set-settings view settings-pointer)))
 (defun webview-new (uri)
   "Returns a webview with the default settings"
-  (let ((settings-pointer (webkit.foreign:webkit-web-settings-new))
-        settings
-        (view (webkit.foreign:webkit-web-view-new)))
-    ;; set settings
-
-    (setq settings (change-settings settings-pointer
-                                    '(("enable-plugings" gobject:+g-type-boolean+ nil)
-                                      ("enable-webgl" gobject:+g-type-boolean+ t)))
-
-    (webkit.foreign:webkit-web-view-set-settings view settings)
-
-
+  (let ((view (webkit.foreign:webkit-web-view-new)))
+    (change-settings view
+                     '(("enable-plugins" nil)
+                       ("enable-webgl" t)))
+      ;; FIXME: Kill scrollbars
+      ;; (gtk:gtk-scrolled-window-set-policy
+      ;;  scrolled ;; (webkit.foreign:webkit-web-view-get-main-frame view)
+      ;;  :never :never)
     (webkit.foreign:webkit-web-view-load-uri view uri)
     (push view *views*) ;; or *ui-views*
     view))
 
-
-;; (defun gval (type content)
-;; (cffi:with-foreign-objects ((value1 'gobject:g-value))
-;;   (gobject:g-value-init value1 gobject:+g-type-boolean+)
-;;   (gobject:g-value-set-boolean value1 nil)
-;;   value1)
-
-;; (let ((p (webkit.foreign:webkit-web-settings-new)))
-;;   (cffi:with-foreign-objects ((value1 'gobject:g-value))
-;;     (gobject:g-value-init value1 gobject:+g-type-boolean+)
-;;     (gobject:g-value-set-boolean value1 nil)
-;;     (gobject:g-object-set-data p "enable-webgl" value1)
-;;     p))
-
-;; http://www.crategus.com/books/cl-cffi-gtk/single-page/index.html#gobject_fun_g-object-class-find-property
-;; (gobject:g-type-from-instance p)
-;; (let ((p (webkit.foreign:webkit-web-settings-new)))
-;;   (cffi:with-foreign-objects ((value1 'gobject:g-value))
-;;     (gobject:g-value-init value1 gobject:+g-type-boolean+)
-;;     (gobject:g-value-set-boolean value1 nil)
-;;     (gobject:g-object-set-data p "enable-webgl" value1)
-;;     p))
-  
-
+;; gobject:get-g-object-for-pointer
+;; gobject:g-type-from-instance
+;; LISPKIT> (gobject:create-gobject-from-pointer (webkit.foreign:webkit-web-view-get-main-frame (first *views*)))
+;; LISPKIT> (gobject:g-type-parent (gobject:g-type-parent (gobject:g-type-from-instance (first *views*))))
 
 (defun win ()
   "Open up the gtk window"
   (within-main-loop
-   (let ((window (make-instance 'gtk-window
-                                :type :toplevel
-                                :title "LispKit"
-                                :default-height 600
-                                :default-width 800
-                                :has-resize-grip nil
-                                :resize-grip-visible nil))
-         (view (webview-new *uri-homepage*))
-         (scrolled (make-instance 'gtk-scrolled-window
-                                  :border-width 0
-                                  :hscrollbar-policy :never
-                                  :vscrollbar-policy :never)))
-     (gobject:g-signal-connect window "destroy"
-                       (lambda (widget)
-                         (declare (ignore widget))
-                         (leave-gtk-main)))
-     (gtk-container-add scrolled view)
-     (gtk-container-add window scrolled)
-     ;; (gtk-scrolled-window-add-with-viewport scrolled view)
-     (gtk-widget-show-all window))))
+    (let ((window (make-instance 'gtk-window
+                                 :type :toplevel
+                                 :title "LispKit"
+                                 :default-height 600
+                                 :default-width 800
+                                 :has-resize-grip nil
+                                 :resize-grip-visible nil))
+          (notebook (make-instance 'gtk-notebook))
+          (scrolled (make-instance 'gtk-scrolled-window
+                                   :border-width 0
+                                   :hscrollbar-policy :never
+                                   :vscrollbar-policy :never)))
+      (gobject:g-signal-connect window "destroy"
+                                (lambda (widget)
+                                  (declare (ignore widget))
+                                  (leave-gtk-main)))
+      (gtk-container-add scrolled 
+          (webview-new *uri-homepage*))
+      (gtk-notebook-append-page notebook scrolled nil)
+      (gtk-container-add window notebook)
+      (gtk-widget-show-all window))))
 
 
+
+
+;; Image startup
 (defun lispkit-internal ()
   "The main function run by starting the image"
   (write-line "Hello World")
