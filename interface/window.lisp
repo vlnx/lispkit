fe7fc086935dd81bf13ccd71f7c01bc98c118c8d
Octantis <none@notset.com>
2013-08-13 14:36:18 -0500

    Working impentation of browser panes. Only as stable as GTK3 and WebKitGTK+1 are.

--- interface/window.lisp
+++ interface/window.lisp
@@ -2,10 +2,6 @@
 (defcfun "gtk_paned_set_position" :void
   (pane pobject)
   (pos :int))
-;; (defun gtk-paned-set-position (posInt)
-;;   (make-instance 'g-object
-;;                  :pointer (%gtk-paned-set-position posInt)))
-
 
 (in-package :lispkit)
 
@@ -40,79 +36,26 @@
          (scrolled-win2 (make-instance 'scrolled-window))
          (view2 (make-instance 'widget :pointer 
                                (tab-new *uri-homepage*)))
-         ;; (vb (make-instance 'v-box :homogeneous nil)))
          (vpaned (make-instance 'v-paned)))
 
+    ;; Commented until scrolling mechanism is complete
     ;; Force the main frame to respond to it's parent container's policy change
-    (cffi:defcallback true :boolean () t)
-    (setf (gsignal (make-instance 'g-object :pointer
-                                  (webkit-web-view-get-main-frame view))
-                   "scrollbars-policy-changed")
-          (cffi:callback true))
-    (setf (policy scrolled-win) '(:never :never))
-
-    ;; (setf (size-request view) '(-1 100))
-    ;; (setf (size-request scrolled-win) '(100 100))
-
-    ;; (cffi:defcallback tabbar_get_preferred_height :void ((widget :pointer)
-    ;;                                                      (minimal :pointer)
-    ;;                                                      (natural :pointer))
-    ;;   (declare (ignore widget))
-    ;;   (write-line "size")
-    ;;   (let ((min (make-instance 'g-value :pointer minimal))
-    ;;         (nat (make-instance 'g-value :pointer natural)))
-    ;;     (setf (value min) 100)
-    ;;     (setf (value nat) 100)))
-    ;; (setf (cffi:foreign-slot-value (pointer view)
-    ;;                                '(:struct widget-class)
-    ;;                                'gtk-cffi::get-preferred-height)
-    ;;       (get-callback 'tabbar_get_preferred_height))
-
-    ;; (add scrolled-win view)
-    ;; (add scrolled-win2 view2)
-    
-    ;; (pack vb scrolled-win :expand nil :fill nil)
-    ;; (pack vb scrolled-win2 :expand t :fill t)
-    ;; (add win vb)
-    ;; (setf *W* scrolled-win)
+    ;; (cffi:defcallback true :boolean () t)
+    ;; (setf (gsignal (make-instance 'g-object :pointer
+    ;;                               (webkit-web-view-get-main-frame view))
+    ;;                "scrollbars-policy-changed")
+    ;;       (cffi:callback true))
+    ;; (setf (policy scrolled-win) '(:never :never))
 
     (add scrolled-win view)
     (add scrolled-win2 view2)
 
-    ;; (add frame1 scrolled-win)
-    ;; (add frame2 scrolled-win2)
-
-    ;; (setf (size-request vpaned) '(200 -1))
-
     (pack vpaned scrolled-win :pane-type 1 :resize t :shrink t)
-    ;; (setf (size-request scrolled-win) '(-1 200))
     (pack vpaned scrolled-win2 :resize t :shrink t)
-    ;; (setf (size-request scrolled-win2) '(-1 200))
     
     (gtk-cffi::gtk-paned-set-position vpaned 200)
 
     (add win vpaned)
 
-    ;; (add win scrolled-win)
-
     (show win :all t)
     (gtk-main)))
-
-
-;; (setf (property view :height-request) 100)
-;; 
-;; (flet ((redraw-size (widget rect &rest data)
-;;          (declare (ignore rect))
-;;          (setf (size-request widget) '(-1 100))
-;;          (write-line "tt")
-;;          t))
-;;   (setf (gsignal view :size-allocate) #'redraw-size))
-;; (flet ((redraw-size (widget context)
-;;          (declare (ignore context))
-;;          (setf (size-request widget) '(-1 100))
-;;          (write-line "tt")
-;;          t)
-;; (cffi:defcallback bottom-redraw :void ()
-;;     (setf (property scrolled-win2 :height-request) 50))
-;; (setf (gsignal view :draw)
-;;       (cffi:callback bottom-redraw))
