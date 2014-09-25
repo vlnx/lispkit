507fda0e9e97a6b780d15e55742be06446a40520
vlnx <vlnx@mail.com>
2014-09-25 17:06:26 -0500

    tab-new: if the current tab is blank, use that existing tab

--- core/tabs.lisp
+++ core/tabs.lisp
@@ -53,34 +53,34 @@
    (widgets-notebook (browser-gtk browser))
    new-index))
 
-(defun tab-new (browser uri &key background initializing-class-notebook)
+(defun current-tab-is-blank-p (browser)
+  (and (current-tab browser)
+       (tab-view (current-tab browser))
+       (string= (property (tab-view (current-tab browser)) :uri)
+                (parse-uri nil))))
+
+(defun tab-new (browser uri &key background)
   "Add a new tab to the notebook"
-  (let ((notebook (widgets-notebook (browser-gtk browser)))
-        (tab (make-instance 'tab :inital-uri uri))
-        new-index)
-    (print notebook)
-    (finish-output)
+  (if (current-tab-is-blank-p browser) ; Use the blank tab if it exists
+      (webkit-web-view-load-uri (tab-view (current-tab browser)) uri)
+      ;; Other wise create a new tab
+      (let ((notebook (widgets-notebook (browser-gtk browser)))
+            (tab (make-instance 'tab :inital-uri uri))
+            new-index)
+        ;; Append new tab to browser's tablist slot
+        (setf (browser-tabs browser)
+              (append (browser-tabs browser)
+                      (list tab)))
+        ;; Show tab container, in order to be added to the notebook
+        (show (tab-scroll tab))
+        ;; Hide native scrollbars now the scroll indicator is working
+        (webview-hide-scrollbars (tab-view tab) (tab-scroll tab))
+        ;; Add tab to notebook
+        (setf new-index (notebook-add-tab notebook
+                                          (tab-scroll tab)))
+        (ui-update browser :add-tab tab)
+        ;; Maybe switch to the new tab
+        (unless background
+          (setf (browser-tabs-current-index browser)
+                new-index)))))
 
-    ;; Append new tab to browser's tablist slot
-    (setf (browser-tabs browser)
-          (append (browser-tabs browser)
-                  (list tab)))
-
-    ;; Show tab container, in order to be added to the notebook
-    (show (tab-scroll tab))
-
-    ;; Hide native scrollbars now the scroll indicator is working
-    (webview-hide-scrollbars (tab-view tab) (tab-scroll tab))
-
-    ;; Add tab to notebook
-    (setf new-index
-          (notebook-add-tab
-           notebook
-           (tab-scroll tab)))
-
-    (ui-update browser :add-tab tab)
-
-    ;; Maybe switch to the new tab
-    (unless background
-      (setf (browser-tabs-current-index browser)
-            new-index))))
