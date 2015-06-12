9b836353ee8642f1375890b701225c7594036bc0
vlnx <vlnx@mail.com>
2015-06-12 00:57:44 -0500

    Remove unused defclass-defuse

--- utils.lisp
+++ utils.lisp
@@ -9,18 +9,6 @@
 (defun as-symbol (string)
   (intern (string-upcase string)))
 
-;; (defmacro defclass-defuse (instance bindings &body body)
-;;   "Given an instance and new bindings, set the slots and use them in the body"
-;;   `(progn
-;;      (mapcar (lambda (l)
-;;                (setf (slot-value ,instance (car l))
-;;                      (eval (car (cdr l)))))
-;;              ',bindings)
-;;      (with-slots
-;;            ,(mapcar (lambda (l) (car l)) bindings)
-;;          ,instance
-;;        ,@body)))
-
 (defun listify (symbol-or-list)
   (if (listp symbol-or-list)
       symbol-or-list
