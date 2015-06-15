c831ac8a7eb1e2daace7b399ff8b999a3df6041d
vlnx <vlnx@mail.com>
2015-06-15 00:40:47 -0500

    clean up core/tabs.lisp

--- core/tabs.lisp
+++ core/tabs.lisp
@@ -1,6 +1,6 @@
 (in-package :lispkit)
-
 ;; Notebook Signals
+
 (defcallback notebook-page-added :void
     ((notebook pobject)
      (child-widget :pointer)
@@ -14,13 +14,9 @@
      (page-num :int))
   (declare (ignore page-num))
   (dmesg "page-removed")
-  (let ((browser (or (find-instance 'of-browser 'from-notebook notebook)
-                     (current-browser)))
+  (let ((browser (find-instance 'of-browser 'from-notebook notebook))
         (removed-tab (find-instance 'of-tab 'from-scrolled-window
                                     child-widget)))
-    ;; Destroy content, guess the content was already free'd
-    ;; (destroy (tab-view removed-tab))
-    ;; (destroy (tab-scroll removed-tab))
     ;; Remove from list
     (setf (browser-tabs browser)
           (remove removed-tab (browser-tabs browser)))
@@ -31,11 +27,11 @@
       (tab-new browser nil))
     ;; Update display
     (ui-update browser :tabs-reset-list t)
-    ;; Update the current tab index, that was changed by removing this page
+    ;; Update the current tab index,
+    ;; possibly changed by removing this page
     (setf (slot-value browser 'tabs-current-index)
           (gtk-notebook-get-current-page notebook))
     (ui-update browser :current-tab t)))
-
 
 (setf (getf *hooks* :switch-page)
       (list #'(lambda (browser)
@@ -43,8 +39,8 @@
                 (ui-update browser :progress t)
                 (ui-update browser :scroll-indicator t)
                 (ui-update browser :link-hover "")
-                (ui-update browser :tabs-update-title
-                           (current-tab browser))
+                (ui-update browser
+                           :tabs-update-title (current-tab browser))
                 (ui-update browser :current-tab t)
                 (ui-update browser :uri t))))
 
@@ -52,8 +48,7 @@
     ((notebook pobject)
      (child-widget pobject)
      (page-num :int))
-  (let ((browser (or (find-instance 'of-browser 'from-notebook notebook)
-                     (current-browser)))
+  (let ((browser (find-instance 'of-browser 'from-notebook notebook))
         (switched-to-tab (find-instance 'of-tab 'from-scrolled-window
                                         child-widget)))
     ;; Update the browser current tab slot
@@ -72,15 +67,15 @@
 
 (defun notebook-add-tab (notebook widget &optional index)
   (let ((i (if index
-               (error "fixme to check if in range")
-               -1))) ; append
+               (error "check if in range") ; TODO:
+               -1))) ; otherwise append
     (gtk-notebook-insert-page notebook
                               widget nil i)))
 
 (defun (setf browser-tabs-current-index) (new-index browser)
-  (gtk-notebook-set-current-page
-   (widgets-notebook (browser-gtk browser))
-   new-index))
+  (gtk-notebook-set-current-page (widgets-notebook
+                                  (browser-gtk browser))
+                                 new-index))
 
 (defun current-tab-is-blank-p (browser)
   (and (current-tab browser)
@@ -91,7 +86,7 @@
 (defun tab-new (browser uri &key background)
   "Add a new tab to the notebook"
   (let (tab new-index)
-    (cond ((current-tab-is-blank-p browser) ; Use the blank tab if it exists
+    (cond ((current-tab-is-blank-p browser) ; Use the blank tab
            (setf tab (current-tab browser))
            (webkit-web-view-load-uri (tab-view tab) uri))
           (t ; Otherwise create a new tab
@@ -117,7 +112,7 @@
 (defun tab-remove (browser tab)
   "Remove a tab"
   (unless (member tab (browser-tabs browser))
-    (error "tab is not in browser-tabs"))
+    (error "tab is not registered to that browser instance"))
   (let ((notebook (widgets-notebook (browser-gtk browser)))
         (index (position tab (browser-tabs browser))))
     (gtk-notebook-remove-page notebook index)))
